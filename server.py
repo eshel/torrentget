09455e689ddaf163d19a2f57e16a5899a93af5dc
Amir Eshel <amir@eshel.com>
2013-09-20 19:02:00 +0300

    Added template to server, made using coroutine

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 import tornado.ioloop
 import tornado.web
+from tornado import gen
+from tornado.options import define, options
+
 
 class MainHandler(tornado.web.RequestHandler):
+    @gen.coroutine
     def get(self):
-        self.write("Hello, world")
+        self.render("templates/index.html")
 
 application = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
-if __name__ == "__main__":
+
+def main():
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
